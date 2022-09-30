314cb9a48005817d9d9d72646928435eb7b8adc7
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-09-30 07:51:29 +0200

    Add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  resources :expenses_groups
-  resources :groups
-  resources :expenses
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
+  resources :groups do
+    resources :expenses
+  end
+    resources :users
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+    # Defines the root path route ("/")
+     root "groups#index"
 end
