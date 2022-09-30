03bc66a431870ae7202582d3a013c75a0e05d00f
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-09-30 08:14:07 +0200

    Fix router

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :expenses_groups
   resources :groups do
     resources :expenses
   end
-    resources :users
-    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  resources :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
-    # Defines the root path route ("/")
-     root "groups#index"
+  # Defines the root path route ("/")
+   root "groups#index"
 end
